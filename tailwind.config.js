3580ebe44145eec4133f07f058addefa0b3f379d
Asad Shiekh <asadshiekh9@gmail.com>
2024-11-04 11:44:11 +0400

    general files
    general files

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '600px',
+          md: '728px',
+          lg: '984px',
+          xl: '1140px',
+          '2xl': '1340px',
+        },
+      },
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        'slide-down': {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(100%)' },
+        },
+      },
+      animation: {
+        'slide-up': 'slide-up 0.5s ease-out',
+        'slide-down': 'slide-down 0.5s ease-out',
+      },
+      
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif'],
+      },
+
+      colors: {
+        primary: '#0f7c90',     // Custom primary color
+        secondary: '#e14958',   // Custom secondary color
+      },
+      backgroundImage:{
+        'homebanner':"url('/images/banner.webp')"
+      }
+    },
+  },
+  plugins: [
+    
+  ],
+};
